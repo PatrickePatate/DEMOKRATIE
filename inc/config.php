fe3878c4d95d0dd908fcd4b87b92372ff83a3a52
Patricke / Lucien <lucienpuget1@gmail.com>
2021-02-04 14:11:43 +0100

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,12 +1,12 @@
 <?php
-$user = "root"; $pass = "";
+$user = ""; $pass = "";
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=vote', $user, $pass);
+    $bdd = new PDO('mysql:host=HOST;dbname=DBNAME', $user, $pass);
 } catch (PDOException $e) {
     die("Erreur Mysql, veuillez contacter un administrateur.");
 }
 
-$tiny_api="gt44dbyveoferxgf1em3t9ww2x4oywvroik3pftdi6dhflp5";
+$tiny_api="XXXXXXX";
 // Vous avez besoin de créer un compte gratuit sur https://tiny.cloud pour récuperer votre clé d'API pour l'éditeur avancé utilisé dans la création des votes.
 $sitename="DEMOKRATIE";
 $siteurl="https://vote.librescommeres.fr/"; // Ceci est l'url de mon instance, à vous de modifier selon l'url de la votre
