0b044973ae865717ed70a49e42da3ba5122b7eaf
Patricke / Lucien <lucienpuget1@gmail.com>
2021-02-04 13:54:32 +0100

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -6,6 +6,8 @@ try {
     die("Erreur Mysql, veuillez contacter un administrateur.");
 }
 
+$tiny_api="XXXXXXXXXXXXXX";
+// Vous avez besoin de créer un compte gratuit sur https://tiny.cloud pour récuperer votre clé d'API pour l'éditeur avancé utilisé dans la création des votes.
 $sitename="DEMOKRATIE";
 $siteurl="https://vote.librescommeres.fr/"; // Ceci est l'url de mon instance, à vous de modifier selon l'url de la votre
 ?>
